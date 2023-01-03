eb4a21f77973019fbba67103b8315767adf6249b
Ahmet Zorlutuna <ahmettayyip3439@gmail.com>
2023-01-03 23:52:25 +0300

    svg icon component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,20 @@
     <Text>Merahaba Dünya</Text>
     <Text tag="p" class="a1">Merahaba Dünya</Text>
     <Text tag="b" size="big">Merahaba Dünya</Text>
+    <HomeSvg />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Text from '@/components/Text.vue'
+import HomeSvg from '@/icons/home.svg'
 
 export default {
   name: 'HomeViews',
   components: {
-    Text
+    Text,
+    HomeSvg
   }
 }
 </script>
